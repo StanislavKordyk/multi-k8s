948b728967bfe1066de83b7150a65b5f4de7d31b
Seneka810 <kordyk.stanislav@gmail.com>
2022-01-30 22:23:25 +0200

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,9 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <h1>Fib calculator version K8s</h1>
+          <Link to="/">Home Link</Link>
+          <Link to="/otherpage">Other Page Link</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
